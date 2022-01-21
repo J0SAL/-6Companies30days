dbaa9489196e071ee174531929625a986c9d4857
Josal <joyalmeida321@gmail.com>
2022-01-21 20:03:02 +0530

    added day 21

--- Intuit/3.cpp
+++ Intuit/3.cpp
@@ -1,4 +1,8 @@
-// submission link: https://practice.geeksforgeeks.org/viewSol.php?subId=28ba59cc26fcffc825a021a5a39c456d&pid=700489&user=josal
+// submission link: https://practice.geeksforgeeks.org/viewSol.php?subId=9b80a1e93bae12e86b5de99fa36e4aa2&pid=700489&user=josal
+/* You are required to complete this function
+which return the required missing number
+if present else return -1*/
+
 int to_int(string s){
     stringstream geek(s);
     int x = 0;
@@ -20,21 +24,16 @@ int missingNumber(const string& str)
         int len = l;
         int i=0;
         int cnt = 0;
-        bool invalid = 0;
         while(i<n and len--){
             curr+=str[i++];
-            // cout<<"ch: "<<len<<endl;
         }
-        // cout<<"l: "<<l<<endl;
         
         prev_i = to_int(curr);
         curr = "";
         int j=i;
-        // cout<<"prev: "<<prev_i<<endl;
         while(j<n){
             curr+=str[j++];
             curr_i = to_int(curr);
-            // cout<<"gen: "<<curr_i<<endl;
             if(prev_i+1 == curr_i){
                 prev_i=curr_i;
                 curr="";
@@ -45,14 +44,8 @@ int missingNumber(const string& str)
                 curr="";
                 prev_i = curr_i;
             }
-            else if(prev_i<curr_i){
-                invalid=1;
-            }
-            // else{
-            //     break;
-            // }
         }
-        if(cnt == 1 and !invalid) return miss_i;
+        if(cnt == 1) return miss_i;
     }
     return -1;
 }
